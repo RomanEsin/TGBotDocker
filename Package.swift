589d6d92a7e00afa8285ec945653f2d749866dfc
Roman Esin <esinromanswift@gmail.com>
2021-07-22 21:20:03 +1000

    asdf

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MaksTG",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .executable(
             name: "MaksTG",
@@ -14,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "TelegramBotSDK", url: "https://github.com/Maxim-Lanskoy/telegram-bot-swift.git", .branch("master")),
+        .package(name: "TelegramBotSDK", url: "https://github.com/RomanEsin/telegram-bot-swift.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
